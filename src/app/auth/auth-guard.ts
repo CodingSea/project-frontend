903edf5b97b36d512983020124555977737c6134
FahadQ <fhdq2003@gmail.com>
2025-09-25 09:19:05 +0300

    fixed auth guard

--- src/app/auth/auth-guard.ts
+++ src/app/auth/auth-guard.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '@app/auth';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate
 {
+  jwtHelper = new JwtHelperService();
+
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(): boolean
   {
-    if (this.authService.isLoggedIn())
+    const token = localStorage.getItem('token');
+
+    if (token && !this.jwtHelper.isTokenExpired(token))
     {
       return true;
     }
