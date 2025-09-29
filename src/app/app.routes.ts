e26c663c8a2b4277a4f42eff34fa1c729b4af0e1
FahadQ <fhdq2003@gmail.com>
2025-09-29 10:03:21 +0300

    added profile page styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { UserComponent } from './user.component/user.component';
 import { Dashboard } from './dashboard/dashboard';
 import { AuthGuard } from './auth/auth-guard';
+import { ProfileComponent } from './profile.component/profile.component';
 
 export const routes: Routes = [
     { path: "", redirectTo: 'auth/signup', pathMatch: "full" },
     { path: "auth/:mode", component: UserComponent },
     { path: 'dashboard', component: Dashboard, canActivate: [AuthGuard] },
+    { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
     { path: '**', component: UserComponent }
 ];
 
---
+++ src/app/profile.component/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sidebar } from "@app/sidebar/sidebar";
+
+@Component({
+  selector: 'app-profile.component',
+  imports: [Sidebar],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+
+}
